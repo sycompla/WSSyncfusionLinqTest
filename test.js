a50c66f37a11dd05f3f7e75dba914ce2635a5308
d7p4n4 <Szappan60>
2021-05-06 15:27:33 +0200

    1.1.20210506.2

--- test.js
+++ test.js
@@ -1,5 +1,43 @@
 import fs from 'fs';
-import * as Enumerable from "linq";
+import Enumerable from "linq";
+import BpmnModdel from 'bpmn-moddle';
+
+const moddle = new BpmnModdel();
+
+async function moddleBasic(flowElements, planeElement, filename) {
+
+    let definitions = moddle.create("bpmn:Definitions");
+
+    let process = moddle.create("bpmn:Process", {
+        id: "process",
+        isExecutable: false,
+        flowElements: flowElements
+    });
+
+    let plane = moddle.create("bpmndi:BPMNPlane", {
+        id: "plane",
+        bpmnElement: process,
+        planeElement: planeElement
+    });
+
+    let diagram = moddle.create("bpmndi:BPMNDiagram", {
+        id: "diagram",
+        plane: plane
+    });
+
+    definitions.get('rootElements').push(process);
+
+    definitions.get('diagrams').push(diagram);
+
+    const {
+        xml: xmlString
+    } = await moddle.toXML(definitions);
+
+    fs.writeFileSync("./" + filename, xmlString);
+
+    //console.log(xmlString);
+
+} // moddleBasic
 
 async function linqTest() {
 
@@ -9,12 +47,42 @@ async function linqTest() {
 
     console.log(nodeArray);
 
-    Enumerable.from(nodeArray)
-        .select(function (node) {
+    let planeElement = await Enumerable.from(nodeArray)
+        .select((node) => {
 
-            console.log(node);
+            if(node.shape.shape == "Event" && node.shape.event.event == "Start") {
 
-        })
+                return moddle.create("bpmndi:BPMNShape", {
+                    id: 'startShape',
+                    bpmnElement: moddle.create("bpmn:StartEvent", {
+                        id: 'startEvent'
+                    }),
+                    bounds: moddle.create("dc:Bounds", {
+                        x: node.offsetX,
+                        y: node.offsetY,
+                        width: node.width,
+                        height: node.height
+                    })
+                })
+
+            }
+
+        }).toArray();
+
+    let flowElements = await Enumerable.from(nodeArray)
+        .select((node) => {
+
+            if(node.shape.shape == "Event" && node.shape.event.event == "Start") {
+
+                return moddle.create("bpmn:StartEvent", {
+                    id: "startEvent"
+                });
+
+            }
+
+        }).toArray();
+
+    moddleBasic(flowElements, planeElement, "startEvent.xml");
 
 } // linqTest
 
